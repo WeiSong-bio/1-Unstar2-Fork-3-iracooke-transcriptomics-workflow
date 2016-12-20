ac14e44f8eca9879ebd85e91c8f73ab310c6aafc
Lauren Taylor <lauren.taylor2@my.jcu.edu.au>
2016-12-20 12:25:05 +1000

    Add working script

--- 01_HISAT.sh
+++ 01_HISAT.sh
@@ -5,12 +5,11 @@
 #PBS -N hisat2
 #PBS -M lauren.taylor2@my.jcu.edu.au
 #PBS -l walltime=1000:00:00
-#PBS -l nodes=1:ppn=24
-#PBS -l pmem=12gb
+#PBS -l nodes=1:ppn=4
+#PBS -l pmem=4gb
 
-module load hisat2
-module load samtools
-for f in *.fastq.gz; do
-  hisat2 -p 24 -x ../grch38/genome -1 $f -S ${f%.fastq.gz}.sam;
-  samtools sort -@ 24 -o ${f%.fastq.gz}.bam ${f%.fastq.gz}.sam;
-done
+
+f=DUMMY
+cd /shares/32/jc320251/untouched_data
+/sw/hisat2/2.0.5/hisat2 -p 4 -x ../grch38/genome -U $f -S ${f%.fastq.gz}.sam;
+/sw/samtools/1.3/AMD/bin/samtools sort -@ 4 -o ${f%.fastq.gz}.bam ${f%.fastq.gz}.sam;
